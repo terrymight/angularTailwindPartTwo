24aef64e0d04f67bff625b8e4b9f530c838b1226
299mold <299mold@gmail.com>
2024-07-20 09:37:50 -0700

    T dev class part one commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { faUserCog,faDashboard ,faBars } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,FontAwesomeModule,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angularTailwind';
+
+  constructor(private router:Router){}
+  open:boolean = false;
+  title = 'T Dev Class';
+  faBars = faBars;
+  faDashboard = faDashboard;
+  faUserCog = faUserCog;
+
+  toggleMenu() {
+    this.open = ! this.open
+  }
+
+  linkAction(){
+    console.log('url pressed');   
+    //this.router.navigate(['/url'])
+  }
 }
